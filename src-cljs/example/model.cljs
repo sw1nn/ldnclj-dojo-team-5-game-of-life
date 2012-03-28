5d906a67acc124756d88758e5660c41524b342b4
Neale Swinnerton <neale@isismanor.com>
2012-03-28 16:11:23 +0100

    Add document comments

--- src-cljs/example/model.cljs
+++ src-cljs/example/model.cljs
@@ -1,4 +1,4 @@
-(ns example.model
+(ns example.model ^{:doc "Holds the 'model' i.e. state of the grid and logic to move the grid to it's next state."}
   (:require [goog.Timer :as Timer]
             [goog.events :as events]
             [example.log :as log]))
@@ -7,7 +7,9 @@
 (def ^:static HEIGHT 50)
 (def ^:static SIZE (* WIDTH HEIGHT))
 
-(defn- fill-random []
+(defn- fill-random
+  "Generate some random data to seed the "
+  []
   (vec (map (fn [_] (if (< 0.6 (rand 1)) :live :dead)) (range SIZE))))
 
 (def ^{:private true} grid (atom (fill-random)))
@@ -18,7 +20,9 @@
 (def timer (atom nil))
 
 
-(defn- adjacent-indices [index]
+(defn- adjacent-indices
+  "Resolves adjacent squares to the given index. NOTE This wraps horizontally but not vertically, you might consider that a bug..."
+  [index]
   (filter #(< -1 % SIZE) ((juxt #(- % 1)
                                 #(+ % 1)
                                 #(- % WIDTH)
@@ -28,7 +32,16 @@
                                 #(- % (dec WIDTH))
                                 #(+ % (dec WIDTH))) index)))
 
-(defn- alive-next-time? [index val]
+(defn- alive-next-time?
+  "Core logic routine. Applies the rules of Conway's game of life.
+
+   1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+   2. Any live cell with two or three live neighbours lives on to the next generation.
+   3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+   4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+  See http://en.wikipedia.org/wiki/Conway's_Game_of_Life for full details "
+  [index val]
   (let [vals-at-indices (map grid-now (adjacent-indices index))
         nlive (count (filter living? vals-at-indices))]
     (cond
@@ -38,16 +51,22 @@
      :else
      :dead)))
 
-(def grid-now)
 
-(defn- update-model []
+
+(def ^{:doc "TODO: better way to handle passing the complete grid into alive-next-time?. This has a global smell to it..."} grid-now)
+
+(defn- update-model
+  "Applies the core logic of alive-next-time? to each cell in the grid"
+  []
   (log/info "update-model")
   
   (binding [grid-now @grid]
     (swap! grid #(vec (map-indexed alive-next-time? %))))
   (log/debug "update-model-end"))
 
-(defn add-listener [f]
+(defn add-listener
+  "Allows interested view to register interest in model changes. TODO: only supports a single listener, should maintain a list."
+  [f]
   (add-watch grid nil
              (fn [k r o n]
                (f n))))
@@ -58,6 +77,8 @@
     (. timer (start))
     (events/listen timer goog.Timer/TICK update-model)))
 
-(defn toggle-run []
+(defn toggle-run
+  "Start the timer. TODO: Expose this up to UI and make it a genuine toggle to start/stop reproduction."
+  []
   (log/info "toggle-run!")
   (start-timer))
--- src-cljs/example/view.cljs
+++ src-cljs/example/view.cljs
@@ -1,4 +1,5 @@
-(ns example.view
+(ns ^{ :doc "UI concerns, displays the grid and listens for changes in the model, updating when it sees them"}
+  example.view
   (:require [example.model :as model]
             [example.log :as log]
             [goog.dom :as dom]
@@ -7,13 +8,17 @@
 (defn- by-id [id]
   (.getElement goog.dom id))
 
-(defn- set-cell-state [x y state]
+(defn- set-cell-state
+  "adds a live CSS class attr to live cells, cells are <TD> elements in an HTML table with id of <x>-<y>"
+  [x y state]
   (if-let [e (by-id (str x "-" y))]
     (if (= state :dead)
       (.removeAttribute e "class")
       (.setAttribute e "class" (name state))))xs)
 
-(defn update-view [grid]
+(defn update-view
+  "callback that is called when the grid state changes, processes the grid sequence and updates the corresponding cells in an HTML table"
+  [grid]
   (log/debug "update-view" grid)
   (doall (map-indexed #(let [x (mod % model/WIDTH)
                              y (Math/floor (/ % model/WIDTH))]
@@ -21,7 +26,9 @@
   (log/debug "update-view-end"))
 
 
-(defn- create-table []
+(defn- create-table
+  "Build an HTML table to act as our grid: TODO hiccup like library for the client side (moustauche does this?)"
+  []
   (set! (.-innerHTML (by-id "content"))  
              (apply str (flatten
                          ["<table id='game-of-life'>"
@@ -35,5 +42,9 @@
   (model/toggle-run)
   (model/add-listener update-view))
 
+
+;; must be after iniialise method. registers a callback on the onload
+;; DOM event. We need to wait for the page to have loaded before we
+;; start accessing the DOM.
 (set! (.-onload js/window) initialise)
 
